e02c984430e133361dff40f1cf889ed44b1f44af
Yesi Meza <yesi.meza10@gmail.com>
2020-12-02 14:25:01 -0700

    fixed boolean issue

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,5 @@
 class School < ApplicationRecord
-  validates_presence_of :name, :number_of_teachers, :is_public
+  inclusion_of :is_public
+  validates_presence_of :name, :number_of_teachers
   has_many :students
 end
