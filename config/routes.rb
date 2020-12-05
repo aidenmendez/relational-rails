0b498ffa7fd66b630b1b16bb7865332007b01f4f
Yesi Meza <yesi.meza10@gmail.com>
2020-12-04 17:40:28 -0700

    added toutes for school

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/schools', to: 'schools#index'
+  get '/schools/new', to: 'schools#new'
+  get '/schools/:id', to: 'schools#show'
+  post '/schools', to: 'schools#create'
+  get '/schools/:id/edit', to: 'schools#edit'
+  patch '/schools/:id', to: 'schools#update'
+  delete '/schools/:id', to: 'schools#destroy' 
+
+  get '/students', to: 'students#index'
 end
