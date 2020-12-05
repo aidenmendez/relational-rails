7b4e4c9657fa32394a8f163e2bcbbd9b800002f6
Aiden Mendez <aidenmendez@gmail.com>
2020-12-05 11:19:07 -0700

    Update garage form fix

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -30,11 +30,11 @@ class GaragesController < ApplicationController
   def update
     garage = Garage.find(params[:id])
     garage.update!({
-      name: params[:garage][:name],
-      address: params[:garage][:address],
-      spot_count: params[:garage][:spot_count],
-      cost: params[:garage][:cost],
-      multi_level: params[:garage][:multi_level] == "1"
+      name: params[:name],
+      address: params[:address],
+      spot_count: params[:spot_count],
+      cost: params[:cost],
+      multi_level: params[:multi_level]
     })
 
     redirect_to "/garages/#{garage.id}"
