a793cd3a8e64786ab8a7a5dead86c460494c0271
Yesi Meza <yesi.meza10@gmail.com>
2020-12-06 01:19:42 -0700

    create an edit and update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,20 @@ class StudentsController < ApplicationController
     # require 'pry'; binding.pry
    @student =  Student.find(params[:id])
   end
+
+  def edit 
+    @student =  Student.find(params[:id])
+  end
+
+  def update  
+    # require 'pry'; binding.pry
+    @student =  Student.find(params[:id])
+    @student.update({
+      name: params[:name], 
+      grade: params[:grade], 
+      in_district: params[:in_district]
+    })
+
+    redirect_to "/students/#{@student.id}"
+  end
 end 
