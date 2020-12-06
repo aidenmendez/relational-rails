dbd275b85fa20223f1c252ae51a0d169ce8b3afd
Yesi Meza <yesi.meza10@gmail.com>
2020-12-06 01:19:18 -0700

    added create and new to schoolstudents

--- app/controllers/school_students_controller.rb
+++ app/controllers/school_students_controller.rb
@@ -3,4 +3,24 @@ class SchoolStudentsController < ApplicationController
     # require 'pry'; binding.pry
     @school = School.find(params[:id])
   end
+
+  def new 
+    @school = School.find(params[:id])
+  end
+
+  def create  
+    # require 'pry'; binding.pry
+    @school = School.find(params[:id])
+    student = Student.new({
+      name: params[:name], 
+      grade: params[:grade], 
+      in_district: params[:in_district], 
+      school_id: @school.id
+    })
+
+    student.save 
+
+    redirect_to "/schools/#{@school.id}/students"
+      # redirect_to "/schools/:id/students"
+  end
 end 
