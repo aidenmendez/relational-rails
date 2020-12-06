c67973789434724bb1d57a8a6f9152188814b021
Aiden Mendez <aidenmendez@gmail.com>
2020-12-06 16:05:10 -0700

    Update attr validation for garage model

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,5 +1,5 @@
 class Garage < ApplicationRecord
-  includes :currently_parked, :multi_level
+  validates_inclusion_of :multi_level, in: [true, false] 
   validates_presence_of :address, :name, :spot_count, :cost
   # dependent: :destroy destroys child elements
   has_many :customers, dependent: :destroy
