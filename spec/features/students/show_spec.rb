702c848f14a89121d41a396167a71813adbc4b75
Yesi Meza <yesi.meza10@gmail.com>
2020-12-07 16:37:00 -0700

    removed comments

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-
-# Then I see the child with that id including the child's attributes:
 RSpec.describe "As a visitor" do
   describe "When I visit '/students/:id" do 
     it " I see a student's attributes" do 
@@ -18,22 +16,11 @@ RSpec.describe "As a visitor" do
           school_id: school_1.id
       )
 
-      # student_2 = Student.create!(
-      #           name: "Tanjiro Kamado", 
-      #           grade: 7,
-      #           in_district: true,
-      #           school_id: school_1.id
-      # )
-
       visit "/students/#{student_1.id}"
-# save_and_open_page
+
       expect(page).to have_content("#{student_1.name}")
       expect(page).to have_content("#{student_1.grade}")
       expect(page).to have_content("#{student_1.in_district}")
-
-      # expect(page).to have_content("#{student_2.name}")
-      # expect(page).to have_content("#{student_2.grade}")
-      # expect(page).to have_content("#{student_2.in_district}")
     end 
     it " I see a link to edit the student" do 
           school_1 = School.create!(
