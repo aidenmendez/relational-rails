2ab9172e8392ebdc8ebaad9dde6b91270bbcc6ae
Yesi Meza <yesi.meza10@gmail.com>
2020-12-07 16:33:41 -0700

    changed syntax to validates inclusion of

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  includes :in_district
+  validates_inclusion_of :in_district, in: [true, false]
   validates_presence_of :name, :grade
   belongs_to :school
 end
