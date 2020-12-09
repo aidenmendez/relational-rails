ca24626e0a1d9e9663d49d52d622a1a38a55f92c
Yesi Meza <yesi.meza10@gmail.com>
2020-12-09 00:49:37 -0700

    created model methods

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,24 @@ class Student < ApplicationRecord
   validates_inclusion_of :in_district, in: [true, false]
   validates_presence_of :name, :grade
   belongs_to :school
-end
+
+  def self.register_sorted
+    Student.all.order(:created_at)
+  end
+
+  def formatted_registration_date
+    created_at.to_formatted_s(:long) 
+  end
+
+  def self.in_district_sorted
+    Student.all.order(in_district: :desc)
+  end
+
+  def self.student_by_grade(number_grade)
+    Student.all.where(grade: number_grade).order(in_district: :desc)
+  end
+
+  def self.student_alphabetical
+    Student.all.order(name: :asc)
+  end
+end 
