07ef6a66e9fe89dd2807bb9f9bb4e37a9477d582
Yesi Meza <yesi.meza10@gmail.com>
2020-12-09 00:49:47 -0700

    ccreated methods for school

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,28 @@ class School < ApplicationRecord
   validates_inclusion_of :is_public, in: [true, false]
   validates_presence_of :name, :number_of_teachers
   has_many :students, dependent: :destroy 
+
+  def registered_students_sorted
+    students.order(created_at: :desc)
+  end
+
+  def self.sorted_schools 
+    order(created_at: :desc)
+  end
+
+  def formatted_creation_date
+    created_at.to_formatted_s(:long) 
+  end
+
+  def student_count
+    students.count
+  end
+
+  def self.is_public_sorted
+    School.all.order(is_public: :desc)
+  end
+
+  def self.number_of_students_sorted
+require 'pry'; binding.pry
+  end
 end
